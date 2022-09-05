084f1c4e873d91664816a4b8c04b715f015a2ce5
ArhipovVS <Arhipof05@yandex.ru>
2022-09-05 23:13:46 +0700

    create and  ralase print array

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿void PrintArray (string [] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+            if (i == 0) Console.Write ("[");
+            if (i < array.Length - 1) Console.Write ($"{array[i],3},");
+            else Console.Write ($"{array[i],3}]");
+    } 
+}
+
+
+string [] array = {"Hellow", "tow", "Word", ":-)"};
+PrintArray (array);
+    
+
+
